9d3a2eefb040b9980fc3fe72e2a2228999ccdae9
dev-jackson <angeljackson0933@gmail.com>
2020-09-16 10:19:35 -0500

    add sql modifed

--- server/shipping_packages.sql
+++ server/shipping_packages.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-09-2020 a las 19:09:37
+-- Tiempo de generación: 16-09-2020 a las 17:18:16
 -- Versión del servidor: 10.4.14-MariaDB
 -- Versión de PHP: 7.4.9
 
@@ -24,11 +24,52 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `business`
+--
+
+CREATE TABLE `business` (
+  `id_business` int(10) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `category_product`
+--
+
+CREATE TABLE `category_product` (
+  `Id_category` int(2) NOT NULL,
+  `games` varchar(50) NOT NULL,
+  `tools` varchar(50) NOT NULL,
+  `decor` varchar(50) NOT NULL,
+  `electronic` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `product`
+--
+
+CREATE TABLE `product` (
+  `id_product` int(10) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` double(10,2) NOT NULL,
+  `quantity` int(6) NOT NULL,
+  `urlImage` varchar(500) NOT NULL,
+  `category` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `user`
 --
 
 CREATE TABLE `user` (
   `id_user` int(10) NOT NULL,
+  `ci` varchar(10) NOT NULL,
   `first_name` varchar(255) NOT NULL,
   `last_name` varchar(255) NOT NULL,
   `password` text NOT NULL,
@@ -36,16 +77,78 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Volcado de datos para la tabla `user`
+--
+
+INSERT INTO `user` (`id_user`, `ci`, `first_name`, `last_name`, `password`, `type_user`) VALUES
+(2, '1515165165', 'sdasdsa', 'dssadas', '$2y$10$lzNNsUqyevGT57cx2VEuWejmOnteP/hOv/qGq9JmEaPy2.rw9e5PW', 'C');
+
+--
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `business`
+--
+ALTER TABLE `business`
+  ADD PRIMARY KEY (`id_business`);
+
+--
+-- Indices de la tabla `category_product`
+--
+ALTER TABLE `category_product`
+  ADD PRIMARY KEY (`Id_category`);
+
+--
+-- Indices de la tabla `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id_product`),
+  ADD KEY `category` (`category`);
 
 --
 -- Indices de la tabla `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id_user`),
-  ADD UNIQUE KEY `id_user` (`id_user`),
-  ADD KEY `id_user_2` (`id_user`);
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `business`
+--
+ALTER TABLE `business`
+  MODIFY `id_business` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `category_product`
+--
+ALTER TABLE `category_product`
+  MODIFY `Id_category` int(2) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `product`
+--
+ALTER TABLE `product`
+  MODIFY `id_product` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`category`) REFERENCES `category_product` (`Id_category`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
